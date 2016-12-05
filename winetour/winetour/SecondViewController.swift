fb5c74365e646ba3de4e8323097027366ae8cb59
Brianna Jones <bfj5889@g.rit.edu>
2016-12-05 18:48:47 -0500

    Empty Swift Project

---
+++ winetour/winetour/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  winetour
+//
+//  Created by Student on 12/5/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
