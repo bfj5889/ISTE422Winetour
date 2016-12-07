f8e9730ec93ff209311db889c2402c41acbddbcb
Brianna Jones <bfj5889@g.rit.edu>
2016-12-06 19:28:34 -0500

    Deleted old code

--- ISTE422Winetour-master/winetour/winetour/CreateAccountVC.swift
+++ ISTE422Winetour-master/winetour/winetour/CreateAccountVC.swift
@@ -8,43 +8,10 @@
 
 import UIKit
 
-class CreateAccountVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    
-    @IBOutlet weak var userTypePickerView: UIPickerView!
-
-    //Create options for the account type picker wheel
-    let userType = ["Public","Winery"];
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return userType.count
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
-        return userType[row]
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        if(row == 0)
-        {
-            self.view.backgroundColor = UIColor.red;
-        }
-        else if(row == 1)
-        {
-            self.view.backgroundColor = UIColor.red;
-        }
-    }
-    
-    //If c=segmented control is picked above information is not needed as well as the info in view did load
+class CreateAccountVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.userTypePickerView.dataSource = self;
-        self.userTypePickerView.delegate = self;
 
         // Do any additional setup after loading the view.
     }
